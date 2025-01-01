88e3cca2b38485cbbb8b0a47e178c8e9a56a1869
Renuka-Khirwadkar123 <renuka.khirwadkarr@gmail.com>
2025-01-01 17:11:27 +0000

    string update

---
+++ stringexercise.js
@@ -0,0 +1,11 @@
+const name="Renuka"
+const repoCount=11
+
+
+console.log(`my name is ${name} and my repo count is ${repoCount}`)
+//The anchor() method of String values creates a string that embeds this string in an <a> element with a name (<a name="...">str</a>).
+console.log(name.anchor("h"))
+//The fontcolor() method of String values creates a string that embeds this string in a <font> element (<font color="...">str</font>), which causes this string to be displayed in the specified font color.
+console.log(name.fontcolor("blue"))
+
+
